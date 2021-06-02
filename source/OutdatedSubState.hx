44715afe4a9beade090ff71f6df4d478a71b9bc6
rayanmargham <rayanmargham4@gmail.com>
2021-06-02 10:20:08 +0100

    sex

---
+++ source/OutdatedSubState.hx
@@ -0,0 +1,46 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxSubState;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+import lime.app.Application;
+
+class OutdatedSubState extends MusicBeatState
+{
+	public static var leftState:Bool = false;
+
+	public static var needVer:String = "IDFK LOL";
+
+	override function create()
+	{
+		super.create();
+		var bg:FlxSprite = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
+		add(bg);
+		var txt:FlxText = new FlxText(0, 0, FlxG.width,
+			"Remember!\n"
+			+ "This version of V.S. Hypno is only a Alpha version!"
+			+"\nWhich means theres no background and theres only one song to play."
+			+"\nWe working on it Ok."
+			+ "!\nPress Space to go for linux memes or ESCAPE to ignore the funny!!",
+			32);
+		txt.setFormat("VCR OSD Mono", 32, FlxColor.WHITE, CENTER);
+		txt.screenCenter();
+		add(txt);
+	}
+
+	override function update(elapsed:Float)
+	{
+		if (controls.ACCEPT)
+		{
+			FlxG.openURL("https://www.youtube.com/watch?v=ezUoiaoQCTs");
+		}
+		if (controls.BACK)
+		{
+			leftState = true;
+			FlxG.switchState(new MainMenuState());
+		}
+		super.update(elapsed);
+	}
+}
